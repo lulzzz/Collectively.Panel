0fc55fa13eb225170d5e5d2c348563375c1a6f33
Patryk Huzarski <phuzarski@gmail.com>
2017-03-28 12:13:40 +0200

    Secure Router by High Order Component needs authenticated

--- src/index.js
+++ src/index.js
@@ -8,7 +8,8 @@ import reduxThunk from 'redux-thunk';
 import App from './components/app';
 import Signin from './components/auth/signin';
 import Signout from './components/auth/signout';
-
+import Dashboard from './components/dashboard';
+import RequireAuth from './components/auth/require_auth';
 import reducers from './reducers';
 
 const createStoreWithMiddleware = applyMiddleware(reduxThunk)(createStore);
@@ -19,6 +20,7 @@ ReactDOM.render(
       <Route path="/" component={App}>
         <Route path="signin" component={Signin} />
         <Route path="signout" component={Signout} />
+        <Route path="dashboard" component={RequireAuth(Dashboard)} />
     </Route>
   </Router>
   </Provider>
