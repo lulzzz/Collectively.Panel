50013bc6f959f462792a2a5e9122c8621625310a
Patryk Huzarski <phuzarski@Patryks-MacBook-Pro.local>
2017-04-19 22:45:15 +0200

    test

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,18 +2,19 @@ import axios from 'axios';
 import { browserHistory } from 'react-router';
 import { AUTH_USER, AUTH_ERROR, UNAUTH_USER } from './types';
 
-const ROOT_URL = 'http://localhost:5000';
-
+const ROOT_URL = 'https://private-anon-608f5abe89-collectively.apiary-mock.com';
+const MAIL = 'phuzarski@gmail.com'
+const PASS = 'secret'
 export function signinUser ({ email, password}) {
   return function(dispatch) {
     //submit email/pass to srv
-    axios.post(`${ROOT_URL}/users`, { email, password })
+    axios.post(`${ROOT_URL}/sign-in`, { email, password })
     .then(response => {
       // update state to indicate user is authenticated
       dispatch({ type: AUTH_USER })
       // SAVE the JWT token
       localStorage.setItem('token', reponse.data.token);
-      // redirect to the route '/dashboard'
+      // redirect to the route '/dashboard' asd
       browserHistory.push('/dashboard');
     })
     .catch(() => {
